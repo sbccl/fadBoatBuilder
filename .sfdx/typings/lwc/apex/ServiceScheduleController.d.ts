bfa75598ee965427cf03eb3dfd8cb55d41732415
Akshay Kumar <akshay@fadealership.com>
2020-04-21 12:55:16 +0530

    Update PartTriggerHandler
    Onchange of part quantity, updatig quantity in woocommerce

--- .sfdx/typings/lwc/apex/ServiceScheduleController.d.ts
+++ .sfdx/typings/lwc/apex/ServiceScheduleController.d.ts
@@ -1,8 +1,11 @@
+declare module "@salesforce/apex/ServiceScheduleController.getUsersSLocation" {
+  export default function getUsersSLocation(): Promise<any>;
+}
 declare module "@salesforce/apex/ServiceScheduleController.getUserList" {
-  export default function getUserList(): Promise<any>;
+  export default function getUserList(param: {sLoaction: any}): Promise<any>;
 }
 declare module "@salesforce/apex/ServiceScheduleController.getUserListSLGroup" {
-  export default function getUserListSLGroup(): Promise<any>;
+  export default function getUserListSLGroup(param: {sLoaction: any}): Promise<any>;
 }
 declare module "@salesforce/apex/ServiceScheduleController.getrecordType" {
   export default function getrecordType(): Promise<any>;
